7b7a6773dacde1fb242a954e29d693db45a11e91
Ezequiel Fernandez <ezequiel004@gmail.com>
2022-09-22 17:45:52 -0300

    Initial commit

---
+++ src/test/java/linkedin/learning/springbatch/SpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package linkedin.learning.springbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
